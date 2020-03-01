4a1cf6a7c6209b8597bf50d105bd50a354022881
Team 1280 <sanramonvalleyrobotics@gmail.com>
2020-02-29 16:53:26 -0800

    updating code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,28 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Authors: Zach LeClaire, Spencer Level, Addison Chen, Laird Wheeler, Ziggy Gardiner*/
+// 2020 BABY
+/*----------------------------------------------------------------------------*/
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-
 public class Robot extends TimedRobot {
+  
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
+
   @Override
   public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+  
   }
+
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-
   }
 
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
   @Override
   public void disabledInit() {
+
   }
 
   @Override
@@ -30,18 +44,25 @@ public class Robot extends TimedRobot {
 
   }
 
+  /**
+   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
+    // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
   }
 
-
+  /**
+   * This function is called periodically during autonomous.
+   */
   @Override
   public void autonomousPeriodic() {
+    
   }
 
   @Override
@@ -51,18 +72,22 @@ public class Robot extends TimedRobot {
     }
   }
 
- 
   @Override
   public void teleopPeriodic() {
-
   }
 
   @Override
   public void testInit() {
+    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
+  /**
+   * This function is called periodically during test mode.
+   */
   @Override
   public void testPeriodic() {
+    //m_robotContainer.testDrive();
   }
-}
+    
+  }
