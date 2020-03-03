45ceb14912d8ace00dfdef15415caef4b6a8a3c9
Zach LeClaire <zacharyjleclaire@gmail.com>
2020-03-02 22:49:11 -0800

    updating motor controller types

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,19 +1,26 @@
 package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.util.LazyVictorSPX;
 import frc.robot.util.Mathz;
 public class Intake extends SubsystemBase{
-   private VictorSP victorIntake = new VictorSP(Constants.PWMIntake1);
-   private VictorSP victorConveyor = new VictorSP(Constants.PWMConveyorBelt);
-   private VictorSP victorHopper = new VictorSP(Constants.PWMHopper);
+   private LazyVictorSPX victorIntake = new LazyVictorSPX(Constants.PWMIntake1);
+   private LazyVictorSPX victorConveyor = new LazyVictorSPX(Constants.PWMConveyorBelt);
+   private LazyVictorSPX victorHopper = new LazyVictorSPX(Constants.PWMHopper);
    private DigitalInput photoElectric1 = new DigitalInput(Constants.DIOphotoElectric);
    private PowerDistributionPanel PDP;
 
    public Intake(PowerDistributionPanel PDP){
     this.PDP = PDP;
+    victorIntake.clearStickyFaults();
+    victorHopper.clearStickyFaults();
+    victorConveyor.clearStickyFaults();
    }
    
     @Override
@@ -27,13 +34,13 @@ public class Intake extends SubsystemBase{
     }
     
   public void setIntakeSpeed(double iSpeed){
-    victorIntake.set(Mathz.VoltageCompensation(iSpeed, PDP.getVoltage(), 12.0));
+    victorIntake.set(ControlMode.PercentOutput, Mathz.VoltageCompensation(iSpeed, PDP.getVoltage(), 12.0));
   }
   public void setConveyor(double cSpeed){
-    victorConveyor.set(Mathz.VoltageCompensation(cSpeed, PDP.getVoltage(), 12.0));
+    victorConveyor.set(ControlMode.PercentOutput, Mathz.VoltageCompensation(cSpeed, PDP.getVoltage(), 12.0));
   } 
   public void setHopper(double hSpeed){
-    victorHopper.set(Mathz.VoltageCompensation(hSpeed, PDP.getVoltage(), 12.0));
+    victorHopper.set(ControlMode.PercentOutput, Mathz.VoltageCompensation(hSpeed, PDP.getVoltage(), 12.0));
   }
 
   public boolean getConveyorSensor(){
